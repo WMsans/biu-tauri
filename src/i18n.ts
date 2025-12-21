f0519d9fdc033d949998e0a56144ea8171da1788
Jeremy <ggajy123@gmail.com>
2025-12-20 22:08:00 -0800

    fix: remove glob

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,15 +2,28 @@ import { initReactI18next } from "react-i18next";
 
 import i18n from "i18next";
 
-const resources = Object.entries(import.meta.glob("@shared/locales/*/translation.json", { eager: true })).reduce(
-  (acc, [path, translation]) => {
-    const lang = path.split("/")[2];
-    acc[lang] = {
-      translation,
-    };
+import { LANGUAGES } from "@shared/locales";
+import en from "@shared/locales/en/translation.json";
+import zhCN from "@shared/locales/zh-CN/translation.json";
+import zhTW from "@shared/locales/zh-TW/translation.json";
+
+// Add new translations here
+const translations: Record<string, unknown> = {
+  en,
+  "zh-CN": zhCN,
+  "zh-TW": zhTW,
+};
+
+const resources = LANGUAGES.reduce(
+  (acc, { value }) => {
+    if (translations[value]) {
+      acc[value] = {
+        translation: translations[value],
+      };
+    }
     return acc;
   },
-  {} as Record<string, { translation: any }>,
+  {} as Record<string, { translation: unknown }>,
 );
 
 i18n.use(initReactI18next).init({
